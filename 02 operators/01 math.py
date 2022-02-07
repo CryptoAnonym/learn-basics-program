0ce5061e7f7d07c3d93d02066ba1f392dc228219
aLitwin <newlitwin@gmail.com>
2022-02-07 11:24:19 +0100

    Init operators Python

---
+++ 02 operators/01 math.py
@@ -0,0 +1,11 @@
+
+a = 12
+b = 3
+
+result = a + b ; print(result)
+result1 = a - b ; print(result1)
+result2 = a * b ; print(result2)
+result3 = a / b ; print(result3)
+result4 = a % 9 ; print(result4) # modulo - reszta z dzielenia
+result5 = a ** b ; print(result5) # do potęgi 
+result6 = a // 5 ; print(result6) # wynik z dzielenia odrzucający resztę 
